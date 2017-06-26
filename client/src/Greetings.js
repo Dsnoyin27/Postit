d465ace6e828ed66cbeb255a34151850c31392a1
Adesina <dsnoyin27@gmail.com>
2017-06-26 22:34:12 +0100

    Implemented links to navigate to different pages

---
+++ client/src/Greetings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Greetings extends React.Component {
+	render(){
+		return(
+			<div className='jumbotron'>
+		      <h1>Welcome to Postit</h1>
+		    </div>
+		 );
+  	}
+}
+export default Greetings;
