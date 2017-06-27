0a61c70df11fc7e2778bfc70bd8a2b055fa37b11
Adesina <dsnoyin27@gmail.com>
2017-06-27 09:41:35 +0100

    Sends data to server via redux thunk action

--- client/src/components/signup/SignupForm.js
+++ client/src/components/signup/SignupForm.js
@@ -17,7 +17,7 @@ class SignupForm extends React.Component {
   }
   onSubmit(e) {
     e.preventDefault();
-    console.log(this.state);
+    this.props.userSignupRequest(this.state);
   }
   render() {
     return (
@@ -78,5 +78,7 @@ class SignupForm extends React.Component {
     );
   }
 }
-
+SignupForm.propTypes = {
+  userSignupRequest: React.PropTypes.func.isRequired
+}
 export default SignupForm;
--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import routes from './routes';
+import thunk from 'redux-thunk';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider } from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 
-ReactDOM.render(<Router history={browserHistory} routes={routes}/>, document.getElementById('root'));
+const store = createStore(
+  (state= {}) => state,
+  applyMiddleware(thunk)
+);
+ReactDOM.render(
+<Provider store={store}>
+<Router history={browserHistory} routes={routes}/></Provider>, document.getElementById('root'));
 registerServiceWorker();
