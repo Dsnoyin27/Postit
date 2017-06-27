3e307188255fcad0c13c1e7c0a5fcaf926c8c615
Adesina <dsnoyin27@gmail.com>
2017-06-27 07:34:41 +0100

    Added signup form and its state

---
+++ client/src/components/signup/SignupForm.js
@@ -0,0 +1,82 @@
+import React from "react";
+
+class SignupForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: ""
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <h1> Join Postit!</h1>
+        <div className="form-group">
+          <label className="control-label">Username</label>
+          <input
+            value={this.state.username}
+            onChange={this.onChange}
+            type="text"
+            name="username"
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label className="control-label">Email</label>
+          <input
+            value={this.state.email}
+            onChange={this.onChange}
+            type="text"
+            name="email"
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label className="control-label">Password</label>
+          <input
+            value={this.state.password}
+            onChange={this.onChange}
+            type="text"
+            name="password"
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label className="control-label">Confirm Password</label>
+          <input
+            value={this.state.confirmPassword}
+            onChange={this.onChange}
+            type="text"
+            name="confirmPassword"
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-primary btn-lg" basic color="purple">
+            Sign up
+          </button>
+        </div>
+        <div><h4>Or</h4></div>
+        <div>
+          <button className="btn btn-primary btn-danger btn-lg">
+            <i aria-hidden="true" class="google plus icon" /> Signup with Google
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default SignupForm;
--- client/src/components/signup/SignupPage.js
+++ client/src/components/signup/SignupPage.js
@@ -1,12 +1,16 @@
-import React from 'react';
-
+import React from "react";
+import SignupForm from "./SignupForm";
 
 class SignupPage extends React.Component {
-	render() {
-		return (
-			<h1>Sign up Page</h1>
-			);
-	}
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-4 col-md-offset-4">
+          <SignupForm />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SignupPage;
